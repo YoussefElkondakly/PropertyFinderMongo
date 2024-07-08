ebef501d8185a865fb17798216464e44bbe8c15b
YoussefElkondakly <youssefelkondakly@gmail.com>
2024-07-08 12:15:31 +0300

    small tricky change
    small tricky change in the count documents while aggregation

--- controller/adminController.js
+++ controller/adminController.js
@@ -65,19 +65,32 @@ agentPhone:1,
           },
         },
     ]);
+     let totalCount = await Ads.aggregate([
+       {
+         $group: {
+           _id: "$agent",
+         },
+       },
+       {
+         $count: "totalCount",
+       },
+     ]);
+     totalCount = totalCount[0]["totalCount"];
     const stats=await paginate(req.query,query)
     if(stats.length===0)return next(new AppError("No data please go to the first page",401))
       const page=req.query.page*1||1
       const limit=req.query.limit*1||10
-    res.status(200).json({
-        status:'success',
-        data:stats,
+    const hasnextpage =
+      page * limit >= totalCount  ? false : true;
+      res.status(200).json({
+        status: "success",
+        data: stats,
         page: page,
         limit: limit,
-        total: stats.length,
-        hasNextPage: page>=1?true:false,
-        hasPreviousPage: page>1?true:false,
-        })
+        total: totalCount,
+        hasNextPage: hasnextpage,
+        hasPreviousPage: page > 1 ? true : false,
+      });
 });
 exports.getRequestsStats = catchAsync(async (req, res, next) => {
   const query = RequestAd.aggregate([
@@ -115,18 +128,31 @@ exports.getRequestsStats = catchAsync(async (req, res, next) => {
       },
     },
   ]);
+let totalCount = await RequestAd.aggregate([
+  {
+    $group: {
+      _id: "$client",
+    },
+  },
+  {
+    $count: "totalCount",
+  },
+]);
+totalCount = totalCount[0]["totalCount"];
   const stats = await paginate(req.query, query);
   if (stats.length === 0)
     return next(new AppError("No data please go to the Previous page", 401));
   const page = req.query.page * 1 || 1;
   const limit = req.query.limit * 1 || 10;
+  const hasnextpage =
+      page * limit >= totalCount  ? false : true;
   res.status(200).json({
     status: "success",
     data: stats,
     page: page,
     limit: limit,
     total: stats.length,
-    hasNextPage: page >= 1 ? true : false,
+    hasNextPage: hasnextpage,
     hasPreviousPage: page > 1 ? true : false,
   });
 });
